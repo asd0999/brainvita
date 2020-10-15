3acfecb832680c187f0c3274e47635eb0917009e
asd0999 <obelixotic@gmail.com>
2020-10-15 15:46:16 -0400

    hide toggle view switch on small screen sizes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // https://codepen.io/amhotdogs/pen/aObgaj
 
 let win = false;
-let rules = "Welcome to Brainvita!<p>This one-person game's objective is to remove every marble except one, and the last marble must end up in the centre<p>To remove a marble, you must move another marble over it and into an empty hollow. The marbles can not move diagonally<p>Toggle switches to enable/disable Training Mode (rewind moves) and change game view.";
+let rules = "Welcome to Brainvita!<p>The objective of this one-person game is to remove all but one marble fom the board and the last marble must end up in the centre hollow<p>To remove a marble, you must move another marble over it and into an empty hollow. The marbles can not move diagonally or skip over multiple marbles<p>The toggle switches enable/disable Training Mode (rewind moves) and change the game view (depending on screen space)";
 let continueOption = "You have an unfinished game. Would like to continue or start a new one?"
     // let marbleDrop = new Audio('assets/marble1.mp3');
 let marbleSound = new Audio('assets/marble2.mp3');
@@ -29,6 +29,9 @@ let screenSize = {};
 
 let myStorage = window.localStorage;
 
+$('.title').on('click', () => {
+    location.reload();
+});
 //back button
 const $back = $('.back');
 $back.on('click', goOneStepBack);
